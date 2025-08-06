8a7c7bb795ff918fa423e8182cd5ea6382051b02
cubocode <cubo.code.dev@gmail.com>
2025-08-06 17:05:28 -0300

    corregido modo clado

--- src/App.js
+++ src/App.js
@@ -22,18 +22,9 @@ const App = () => {
   const storedTheme = useSelector((state) => state.theme)
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
-    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
-    if (theme) {
-      setColorMode(theme)
-    }
-
-    if (isColorModeSet()) {
-      return
-    }
-
-    setColorMode(storedTheme)
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+    // Forzar siempre el tema oscuro
+    setColorMode('dark')
+  }, [setColorMode]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <HashRouter>
